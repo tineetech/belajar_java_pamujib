5e806206ca2be97e5d2f23cdf78c7d647fbfc7d0
zeru-program <justinebogor0609@gmail.com>
2025-08-06 09:28:01 +0700

    update: bab 4-8

---
+++ src/belajar_java_pamujib_p1/Bab7.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package belajar_java_pamujib_p1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Bab7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //        String nama[] = new String[3];
+//        nama[0] = "kaila";
+//        nama[1] = "3D";
+//        nama[2] = "sally";
+//
+//        System.out.println("Index 0 : "+nama[0]);
+//        System.out.println("Index 1 : "+nama[1]);
+//        System.out.println("Index 2 : "+nama[2]);
+//
+//       nama[0] = JOptionPane.showInputDialog(null,"Nama :","index ke-0",JOptionPane.INFORMATION_MESSAGE);
+//       nama[1] = JOptionPane.showInputDialog(null,"Nama :","index ke-1",JOptionPane.INFORMATION_MESSAGE);
+//       nama[2] = JOptionPane.showInputDialog(null,"Nama :","index ke-2",JOptionPane.INFORMATION_MESSAGE);
+//
+//       JOptionPane.showMessageDialog(null, nama[0]+", "+nama[1]+","+nama[2]);
+
+String umur[][] = new String[2][2];
+umur[0][0] = "kaila";
+umur[0][1] = "16";
+umur[1][0] = "ajeng";
+umur[1][1] = "16";
+
+System.out.println("Nama : "+umur[0][0]+" umur : "+umur[0][1]);
+System.out.println("Nama : "+umur[1][0]+" umur : "+umur[1][1]);
+
+    }
+    
+}
